85c14860231e363f1b7ca5614cef3d2327e3978f
Michael <mike@solinta.co.za>
2019-07-02 17:13:57 +0200

    Update Artwork nav title

--- src/app/modules/Art/components/art-home/art-home.component.ts
+++ src/app/modules/Art/components/art-home/art-home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-art-home',
@@ -8,12 +7,9 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ArtHomeComponent implements OnInit {
 
-  currentType: string;
-
-  constructor(private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit() {
-    this.currentType = this.route.snapshot.params['type'];
   }
 
 }
